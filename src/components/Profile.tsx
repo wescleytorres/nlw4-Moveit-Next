9071f7ab8d03ed18547b7e00a687ceb2244ab97e
Wescley Torres <jewescley@gmail.com>
2021-02-25 13:05:45 -0300

    4/5 Next-NLW

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,22 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
-
 export function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/wescleytorres.png" alt="Wescley Torres" />
       <div>
         <strong>Wescley Torres</strong>
+
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
-          </p>
+          Level {level}
+        </p>
       </div>
     </div>
   )
-}   
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
 import Head from 'next/head'
 
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/Countdown";
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Profile";
-import { ChallengeBox } from "../components/ChallengeBox";
-
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { ExperienceBar } from '../components/ExperienceBar'
+import { ChallengeBox } from '../components/ChallengeBox'
+import { Countdown } from '../components/Countdown'
+import { Profile } from '../components/Profile'
 
 import styles from '../styles/pages/Home.module.css'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Inicio | move.it</title>
+        <title>Início | move.it</title>
       </Head>
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
